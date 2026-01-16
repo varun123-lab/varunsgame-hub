996d0fa904e9957af7b9a7a61998c8878babb441
Varun Barmavat <varunbarmavat@gmail.com>
2026-01-16 19:37:03 +0000

    Remove theme switcher - single modern theme only

--- script.mjs
+++ script.mjs
@@ -1,13 +1,11 @@
 import { initGameTimeTracking, updateStatsDisplay } from './gameStats.mjs';
 import { generateGameLinks } from './generateGames.mjs';
 import { initInputHandler } from './inputHandler.mjs';
-import { initThemeSwitcher } from './themes.mjs';
 import {logVisit} from './visitsCounter.mjs';
 
 
 window.onload = function () {
     generateGameLinks();
-    initThemeSwitcher();
     initGameTimeTracking();
     updateStatsDisplay();
     initInputHandler();
